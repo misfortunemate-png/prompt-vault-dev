6cd145149d291000b7426474ec3973d613548e6f
misfortunemate-png <misfortunemate@gmail.com>
2026-09-09 16:43:41 +0900

    test: register #16 and #20 verifiers

--- tests/issues/manifest.mjs
+++ tests/issues/manifest.mjs
@@ -1,5 +1,9 @@
+import pv016 from './pv-016-backend-identity.mjs';
+import pv020 from './pv-020-save-partial-failure.mjs';
 import pv023 from './pv-023-cors-preview.mjs';
 
 export const issueVerifiers = Object.freeze({
+  [pv016.issue]: pv016,
+  [pv020.issue]: pv020,
   [pv023.issue]: pv023,
 });
