a02219578216bb436a8fb2ba0357e2dfa289578a
misfortunemate-png <misfortunemate@gmail.com>
2026-09-12 04:34:21 +0900

    test: register Phase 6B scanner verifier

--- tests/issues/manifest.mjs
+++ tests/issues/manifest.mjs
@@ -1,9 +1,11 @@
 import pv016 from './pv-016-backend-identity.mjs';
 import pv020 from './pv-020-save-partial-failure.mjs';
 import pv023 from './pv-023-cors-preview.mjs';
+import pv040 from './pv-040-scanner-read-failure.mjs';
 
 export const issueVerifiers = Object.freeze({
   [pv016.issue]: pv016,
   [pv020.issue]: pv020,
   [pv023.issue]: pv023,
+  [pv040.issue]: pv040,
 });
